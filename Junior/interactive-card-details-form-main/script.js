6a627cf3b912ca69971cd7c4e0b0abf3489d0ad5
Mugees Ahmad <shmugeesahmad@gmail.com>
2023-01-27 19:57:50 +0500

    Finished everything

--- Junior/interactive-card-details-form-main/script.js
+++ Junior/interactive-card-details-form-main/script.js
@@ -23,7 +23,6 @@ const inputYear = document.querySelector('[data-year]');
 const inputMonth = document.querySelector('[data-month]');
 const inputCVC = document.querySelector('[data-cvc]');
 
-
 //  Buttons
 
 const confirmButton = document.querySelector('.buttonForm');
@@ -33,16 +32,40 @@ const continueButton = document.querySelector('.buttonContinue');
 
 const lightGrey = 'var(--light-grey-violet)';
 const darkGrey = 'var(--very-dark-grey-violet)';
+
 // Funtions on click
+
 confirmButton.addEventListener('click', 
-    () => {
-        form.style.display = 'none';
-        completed.style.display = 'flex';
+() => {
+        // console.clear();
+        let counter = 0;
+        let arrays = [inputName, inputNumber, inputMonth, inputYear , inputCVC];
+        let errorList = [errorName, errorNumber, errorMonth, errorYear, errorCVC];
+        let errorMSG = [`Name can't be blank`, `Card number can't be blank`, `Month can't be blank` , `Year can't be blank`, `CVC can't be blank`];
+        let functions = [nameFunction, numberFunction, monthFunction, yearFunction, cvcFunction];
+        while(counter < arrays.length) {
+            if (arrays[counter].value == ''){
+                arrays[counter].style.borderColor = lightGrey;
+                errorList[counter].style.display = 'block';
+                errorList[counter].innerText = errorMSG[counter];
+                console.log(arrays[counter]);
+                console.log(arrays[counter].value);
+            }
+            else if (!(functions[counter]())){
+                functions[counter]();
+            }
+            else if(nameFunction() && numberFunction() && monthFunction() && yearFunction() && cvcFunction()) {
+                form.style.display = 'none';
+                completed.style.display = 'flex';        
+            }
+            counter += 1;
+        }
     }
 )
 
 continueButton.addEventListener('click', 
     () => {
+        let arrays = [inputName, inputNumber, inputMonth, inputYear , inputCVC];
         form.style.display = 'block';
         completed.style.display = 'none';
         cardName.innerText = 'Jane Appleseed';
@@ -55,49 +78,74 @@ continueButton.addEventListener('click',
         inputMonth.value = '';
         inputYear.value = '';
         inputCVC.value = '';
+        arrays.forEach((input) =>{
+            input.style.borderColor = lightGrey;
+        })
     }
 )
-
 
 // Validity funtions
 
 function nameFunction(){
-    if(inputName.value.match(/^[a-zA-Z]+$/g) && inputName.value != ''){
-        inputName.style.borderColor = 'var(--very-dark-grey-violet)';
+    if(inputName.value == ''){
+        inputName.style.borderColor = lightGrey;
         errorName.style.display = 'none';
-        return true;
-    } else if(inputName.value == ''){
-        inputName.style.borderColor = 'var(--light-grey-violet)';
-        errorName.style.display = 'none';
+        cardName.innerText = 'Jane Appleseed';
         return false;
-    } else{
+    }
+
+    else if (inputName.value.match(/\d/)){
         inputName.style.borderColor = 'red';
         errorName.style.display = 'block';
         errorName.innerText = 'Name can only consist of alphabetic data!';
+        return false;
     }
+
+    else if(inputName.value.length < 3){
+        inputName.style.borderColor = 'red';
+        errorName.style.display = 'block';
+        errorName.innerText = 'Name must consist of more than two letters at least!';
+        cardName.innerText = 'Jane Appleseed';
+        return false;
+    }
+    else if(inputName.value.match(/^[a-zA-Z ]+$/g)){
+        inputName.style.borderColor = darkGrey;
+        errorName.style.display = 'none';
+        cardName.innerText = inputName.value;
+        return true;
+    } 
+
 }
 
 function numberFunction(){
-    if(inputNumber.value.match(/^\d{16}$/g) && inputNumber.value != ''){
-        inputNumber.style.borderColor = 'var(--very-dark-grey-violet)';
+    if(inputNumber.value.match(/^\d{16}$/g)){
+        inputNumber.style.borderColor = darkGrey;
         errorNumber.style.display = 'none';
+        cardNumber.innerText = inputNumber.value.match(/.{4}/g).join(' ');
         return true;
-    } else if(inputNumber.value == ''){
-        inputNumber.style.borderColor = 'var(--light-grey-violet)';
-        errorNumber.style.display = 'none';
-    } else if(inputNumber.value.match(/\w/)){
-        inputNumber.style.borderColor = 'red';
-        errorNumber.style.display = 'block';
-        errorNumber.innerText = 'Card number can only consist of numeric data!';
     }
- else if(inputNumber.value.length < 16){
-        inputNumber.style.borderColor = 'red';
-        errorNumber.style.display = 'block';
-        errorNumber.innerText = `Card number can't be shorter than 16 numbers!`;
-    } else if(inputNumber.value.length > 16){
-        inputNumber.style.borderColor = 'red';
-        errorNumber.style.display = 'block';
-        errorNumber.innerText = `Card number can't be longer than 16 numbers!`;
+
+    else if(inputNumber.value == ''){
+            inputNumber.style.borderColor = lightGrey;
+            errorNumber.style.display = 'none';
+            cardNumber.innerHTML = '0000 0000 0000 0000';
+            return false;
+    }
+
+    else {
+            inputNumber.style.borderColor = 'red';
+            errorNumber.style.display = 'block';
+            cardNumber.innerHTML = '0000 0000 0000 0000';
+            if(inputNumber.value.match(/[A-Z]/i)){
+              errorNumber.innerText = 'Card number can only consist of numeric data!';
+            }
+            else if(inputNumber.value.length < 16){
+                errorNumber.innerText = `Card number can't be shorter than 16 numbers!`;
+            } 
+            else if(inputNumber.value.length > 16){
+                errorNumber.innerText = `Card number can't be longer than 16 numbers!`;
+            }
+            return false;
     }
 }
 
@@ -109,7 +157,11 @@ function monthFunction(){
         return false;
     } else if (Number(inputMonth.value) > 0 && Number(inputMonth.value) <= 12){
         inputMonth.style.borderColor = 'var(--very-dark-grey-violet)';
-        month.innerText = inputMonth.value;
+        if(Number(inputMonth.value) < 10){
+            month.innerText = '0'+ Number(inputMonth.value);
+        }else{
+            month.innerText = Number(inputMonth.value);
+        }
         errorMonth.style.display = 'none';
         return true;
     } else {
@@ -137,7 +189,11 @@ function yearFunction(){
         return false;
     } else if (Number(inputYear.value) <= 99){
         inputYear.style.borderColor = 'var(--very-dark-grey-violet)';
-        year.innerText = inputYear.value;
+        if(Number(inputYear.value) < 10){
+            year.innerText = '0'+ Number(inputYear.value);
+        }else{
+            year.innerText = Number(inputYear.value);
+        }
         errorYear.style.display = 'none';
         return true;
     } else {
@@ -165,7 +221,16 @@ function cvcFunction(){
         return false;
     } else if (Number(inputCVC.value) <= 999){
         inputCVC.style.borderColor = 'var(--very-dark-grey-violet)';
-        cvc.innerText = inputCVC.value;
+        if (Number(inputCVC.value < 10)){
+            cvc.innerText = '00' + Number(inputCVC.value);
+        }
+        else if(Number(inputCVC.value < 100)){
+            cvc.innerText = '0' + parseInt(inputCVC.value);
+            console.log(Number(inputCVC.value))
+        }
+        else{
+            cvc.innerText = Number(inputCVC.value);
+        }
         errorCVC.style.display = 'none';
         return true;
     } else {
@@ -182,5 +247,4 @@ function cvcFunction(){
         errorCVC.innerText = err;
         return false;
     }
-    
 }
